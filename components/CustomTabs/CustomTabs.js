f09372117179fc6b660106e45a9577d37921eb2e
doan199x <69084195+doan199x@users.noreply.github.com>
2021-06-21 18:04:01 +0700

    update cus comp

--- components/CustomTabs/CustomTabs.js
+++ components/CustomTabs/CustomTabs.js
@@ -1,13 +1,21 @@
 import React from "react";
+
+
 // nodejs library that concatenates classes
 import classNames from "classnames";
+
+
 // nodejs library to set properties for components
 import PropTypes from "prop-types";
+
+
 
 // material-ui components
 import { makeStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+
+
 // core components
 import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
@@ -27,6 +35,7 @@ export default function CustomTabs(props) {
     [classes.cardTitle]: true,
     [classes.cardTitleRTL]: rtlActive,
   });
+
   return (
     <Card plain={plainTabs}>
       <CardHeader color={headerColor} plain={plainTabs}>
